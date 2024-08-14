8fb55ce5d362c222e2c0b7d3a4929a174377b9fd
Kevin Comas <kevin@madnet.madnet>
2024-08-13 21:56:27 -0400

    op calls

--- src/code.c
+++ src/code.c
@@ -36,12 +36,16 @@ static const char *op_c_str[] = {
     "ZOO",
     "CST",
     "ADD",
-    "NOT"
+    "SUB",
+    "EQ",
+    "NOT",
+    "OR"
+
 };
 
 const char *op_c_get_str(op_c oc) {
     const char *s = "INVALID";
-    if (oc >= OP_C(EFN) && oc <= OP_C(NOT)) s = op_c_str[oc];
+    if (oc >= OP_C(EFN) && oc <= OP_C(OR)) s = op_c_str[oc];
     return s;
 };
 
@@ -58,6 +62,7 @@ void code_p(const code_st *const cs, const code *const c, size_t idnt) {
                 putchar('\n');
                 code_p(cs, c->ops[i].od.of->cond, idnt + 1);
                 code_p(cs, c->ops[i].od.of->body, idnt + 2);
+                PCX(' ', idnt + 1);
                 break;
             case TYPE(U3):
                 printf(",%d", c->ops[i].od.u3);
@@ -95,16 +100,16 @@ static code_stat code_gen_lst(code_st *const cs, const lst_node *const lst, code
     return CODE_STAT(OK);
 }
 
-static code_stat code_gen_if(code_st *const cs, const if_node *const in, code *c) {
+static code_stat code_gen_if(code_st *const cs, const ast *const a, code *c) {
     code_stat cstat;
-    if_itm *h = in->h;
+    if_itm *h = a->n.in->h;
     op_if *of = op_if_i(CODE_I_SIZE);
     while (h) {
         if (h->cond) IFCGEN(code_gen, cs, h->cond, of->cond);
         IFCGEN(code_gen_lst, cs, h->body, of->body);
         h = h->next;
     }
-    OP_A(c, COND, COND, { .of = of }, NULL);
+    OP_A(c, COND, COND, { .of = of }, a);
     return CODE_STAT(OK);
 }
 
@@ -142,10 +147,9 @@ static code_stat code_gen_op(code_st *const cs, const ast *const a, code *c) {
     op_node *opn = a->n.op;
     var_node *var;
     type_node *tl, *tr;
-    if (opn->ot != OP_TYPE(ASS)) if (opn->l) IFCGEN(code_gen, cs, opn->l, c);
-    if (opn->ot != OP_TYPE(CST)) if (opn->r) IFCGEN(code_gen, cs, opn->r, c);
     switch (opn->ot) {
         case OP_TYPE(ASS):
+            IFCGEN(code_gen, cs, opn->r, c);
             if (opn->l->at != AST_TYPE(VAR)) return CODE_STAT(INV_L_ASS);
             var = opn->l->n.var;
             switch (var->vt) {
@@ -162,20 +166,69 @@ static code_stat code_gen_op(code_st *const cs, const ast *const a, code *c) {
             }
             break;
         case OP_TYPE(CST):
+            IFCGEN(code_gen, cs, opn->l, c);
             OP_P_INT(opn, cs, r, c);
             if (opn->r->at == AST_TYPE(FN)) return code_gen(cs, opn->r, c);
             // TODO dynamic cast
             break;
         case OP_TYPE(ADD):
+            IFCGEN(code_gen, cs, opn->l, c);
+            IFCGEN(code_gen, cs, opn->r, c);
             OP_P_INT(opn, cs, l, c);
             OP_P_INT(opn, cs, r, c);
             OP_A(c, ADD, OP, { .t = opn->ret->t }, a);
             break;
+        case OP_TYPE(SUB):
+            IFCGEN(code_gen, cs, opn->l, c);
+            IFCGEN(code_gen, cs, opn->r, c);
+            OP_P_INT(opn, cs, l, c);
+            OP_P_INT(opn, cs, r, c);
+            OP_A(c, SUB, OP, { .t = opn->ret->t }, a);
+            break;
+        case OP_TYPE(EQ):
+            IFCGEN(code_gen, cs, opn->l, c);
+            if (!(tl = ast_gtn(opn->l))) return CODE_STAT(OP_NO_T_L);
+            OP_A(c, ZOO, OP, { .t = tl->t }, a);
+            IFCGEN(code_gen, cs, opn->r, c);
+            if (!(tr = ast_gtn(opn->r))) return CODE_STAT(OP_NO_T_R);
+            OP_A(c, ZOO, OP, { .t = tr->t }, a);
+            OP_A(c, EQ, OP, { .t = TYPE(BL) }, a);
+            break;
         case OP_TYPE(NOT):
+            IFCGEN(code_gen, cs, opn->r, c);
             if (!(tr = ast_gtn(opn->r))) return CODE_STAT(OP_NO_T_R);
             OP_A(c, ZOO, OP, { .t = tr->t }, a);
             OP_A(c, NOT, OP, { .t = TYPE(BL) }, a);
             break;
+        case OP_TYPE(OR):
+            IFCGEN(code_gen, cs, opn->l, c);
+            if (!(tl = ast_gtn(opn->l))) return CODE_STAT(OP_NO_T_L);
+            OP_A(c, ZOO, OP, { .t = tl->t }, a);
+            IFCGEN(code_gen, cs, opn->r, c);
+            if (!(tr = ast_gtn(opn->r))) return CODE_STAT(OP_NO_T_R);
+            OP_A(c, ZOO, OP, { .t = tr->t }, a);
+            OP_A(c, OR, OP, { .t = TYPE(BL) }, a);
+            break;
+    }
+    return CODE_STAT(OK);
+}
+
+code_stat code_gen_call(code_st *const cs, const ast *const a, code *c) {
+    code_stat cstat;
+    call_node *cn = a->n.cn;
+    op_node *op = cn->tgt->n.op;
+    switch (cn->tgt->at) {
+        case AST_TYPE(OP):
+            if (cn->args->h) {
+                op->l = cn->args->h->a;
+                if (cn->args->h->next) op->r = cn->args->h->next->a;
+            }
+            IFCGEN(code_gen_op, cs, cn->tgt, c);
+            op->ret = NULL;
+            op->l = op->r = NULL;
+            break;
+        default:
+            return CODE_STAT(INV_CALL_TGT);
     }
     return CODE_STAT(OK);
 }
@@ -205,15 +258,26 @@ code_stat code_gen(code_st *const cs, const ast *const a, code *c) {
             break;
         case AST_TYPE(OP): return code_gen_op(cs, a, c);
         case AST_TYPE(LST): return code_gen_lst(cs, a->n.lst, c);
-        case AST_TYPE(IF): return code_gen_if(cs, a->n.in, c);
+        case AST_TYPE(IF): return code_gen_if(cs, a, c);
         case AST_TYPE(FN):
             cfn = code_i(CODE_I_SIZE);
             IFCGEN(code_gen_fn, cs, a->n.fn, cfn);
             OP_A(c, PV, FN, { .c = cfn }, a);
             break;
-        // TODO
+        case AST_TYPE(CALL): return code_gen_call(cs, a, c);
         case AST_TYPE(RET):
-            IFCGEN(code_gen, cs, a->n.ret->a, c);
+            if (a->n.ret->a->at == AST_TYPE(VAL)) {
+                switch (a->n.ret->a->n.val->tn->t) {
+                    case TYPE(INT):
+                        if ((cstat = p_int(cs, TYPE(I6), a->n.ret->a, c)) != CODE_STAT(OK)) return cstat;
+                        break;
+                    case TYPE(FLT):
+                    case TYPE(STR):
+                        break; // TODO
+                    default:
+                        return CODE_STAT(NO_OP_FOR_RET_VAL_T);
+                }
+            } else IFCGEN(code_gen, cs, a->n.ret->a, c);
             OP_A(c, RFN, VD, {}, a);
             break;
         case AST_TYPE(VAR):
