7868e371bad3e12a79c90e443cc754722f112f4f
Kevin Comas <kevin@madnet.madnet>
2024-08-17 16:06:36 -0400

    build stk for fn jit

--- src/jit.c
+++ src/jit.c
@@ -3,4 +3,17 @@
 
 extern inline jit *jit_i(void);
 
+extern inline void jit_a(jit *const j, uint8_t b);
+
 extern inline void jit_f(jit *j);
+
+extern inline void fn_stk_i(fn_stk *const stk);
+
+extern inline void fn_stk_a(fn_stk *const stk, const code *const c);
+
+void fn_stk_b(fn_stk *const stk, const code *const c) {
+    for (size_t i = 0; i < c->len; i++) {
+        if (c->ops[i].oc == OP_C(PV) && c->ops[i].ot == TYPE(FN)) fn_stk_b(stk, c->ops[i].od.c);
+    }
+    fn_stk_a(stk, c);
+}
--- src/jit.h
+++ src/jit.h
@@ -16,7 +16,32 @@ inline jit *jit_i(void) {
     return j;
 }
 
+inline void jit_a(jit *const j, uint8_t b) {
+    j->a[j->len++] = b;
+}
+
 inline void jit_f(jit *j) {
     munmap(j->a, j->size);
     free(j);
 }
+
+#ifndef FN_STK_SIZE
+    #define FN_STK_SIZE 20
+#endif
+
+typedef struct {
+    size_t len, size;
+    const code *fn[FN_STK_SIZE];
+} fn_stk;
+
+inline void fn_stk_i(fn_stk *const stk) {
+    stk->len = 0;
+    stk->size = FN_STK_SIZE;
+    memset(stk->fn, 0, sizeof(code*) * FN_STK_SIZE);
+}
+
+inline void fn_stk_a(fn_stk *const stk, const code *const c) {
+    stk->fn[stk->len++] = c;
+}
+
+void fn_stk_b(fn_stk *const stk, const code *const c); // create stack from code scan
