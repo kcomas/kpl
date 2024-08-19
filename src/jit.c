1b88d2738a3bb1c6ed23e8ca25c39add5fa8fac4
Kevin Comas <kevin@madnet.madnet>
2024-08-18 23:01:03 -0400

    resize jit block

--- src/jit.c
+++ src/jit.c
@@ -1,20 +1,33 @@
 
 #include "jit.h"
 
-extern inline jit *jit_i(void);
-
-extern inline void jit_a(jit *const j, uint8_t b);
-
-extern inline void jit_f(jit *j);
-
 extern inline fn_stk *fn_stk_i(size_t size);
 
-extern inline void fn_stk_a(fn_stk **stk, const code *const c);
+extern inline void fn_stk_a(fn_stk **stk, code *const c);
 
-void fn_stk_b(fn_stk **stk, const code *const c) {
+void fn_stk_b(fn_stk **stk, code *const c) {
     for (size_t i = 0; i < c->len; i++)
         if (c->ops[i].oc == OP_C(PV) && c->ops[i].ot == TYPE(FN)) fn_stk_b(stk, c->ops[i].od.c);
     fn_stk_a(stk, c);
 }
 
 extern inline void fn_stk_f(fn_stk *f);
+
+extern inline jit *jit_i(int np);
+
+extern inline void jit_f(jit *j);
+
+extern inline void jit_a(jit **j, uint8_t b);
+
+jit_stat jit_code(mod *const m, code *const c, jit **j) {
+    return JIT_STAT(OK);
+}
+
+jit_stat jit_stk(mod *const m, fn_stk *const stk, jit **j) {
+    jit_stat jstat;
+    for (size_t i = 0; i < stk->len; i++) {
+        stk->fn[i]->jf = (jit_fn*) &(*j)->a[(*j)->len];
+        if ((jstat = jit_code(m, stk->fn[i], j)) != JIT_STAT(OK)) return jstat;
+    }
+    return JIT_STAT(OK);
+}
--- src/jit.h
+++ src/jit.h
@@ -2,28 +2,14 @@
 #pragma once
 
 #include "kpl.h"
+#include "mod.h"
 #include "code.h"
 
-typedef struct _jit {
-    int len, size; // compat with getpagesize
-    uint8_t *a; // address
-} jit;
+#define JIT_STAT(N) JIT_STAT_##N
 
-inline jit *jit_i(void) {
-    jit *j = calloc(1, sizeof(jit));
-    j->size = getpagesize();
-    j->a = mmap(NULL, j->size, PROT_READ | PROT_WRITE | PROT_EXEC, MAP_ANONYMOUS | MAP_PRIVATE, -1, 0);
-    return j;
-}
-
-inline void jit_a(jit *const j, uint8_t b) {
-    j->a[j->len++] = b;
-}
-
-inline void jit_f(jit *j) {
-    munmap(j->a, j->size);
-    free(j);
-}
+typedef enum {
+    JIT_STAT(OK)
+} jit_stat;
 
 #ifndef FN_STK_SIZE
     #define FN_STK_SIZE 10
@@ -31,7 +17,7 @@ inline void jit_f(jit *j) {
 
 typedef struct {
     size_t len, size;
-    const code *fn[];
+    code *fn[];
 } fn_stk;
 
 inline fn_stk *fn_stk_i(size_t size) {
@@ -44,7 +30,7 @@ inline fn_stk *fn_stk_i(size_t size) {
     #define FN_STK_RSIZE 2
 #endif
 
-inline void fn_stk_a(fn_stk **stk, const code *const c) {
+inline void fn_stk_a(fn_stk **stk, code *const c) {
     if ((*stk)->len == (*stk)->size) {
         fn_stk *ns = fn_stk_i((*stk)->size * FN_STK_RSIZE);
         for (size_t i = 0; i < (*stk)->len; i++) ns->fn[ns->len++] = (*stk)->fn[i];
@@ -54,8 +40,49 @@ inline void fn_stk_a(fn_stk **stk, const code *const c) {
     (*stk)->fn[(*stk)->len++] = c;
 }
 
-void fn_stk_b(fn_stk **stk, const code *const c); // create stack from code scan
+void fn_stk_b(fn_stk **stk, code *const c); // create stack from code scan
 
 inline void fn_stk_f(fn_stk *f) {
     free(f);
 }
+
+typedef struct _jit {
+    int np, len, size; // compat with getpagesize
+    uint8_t *a; // address
+} jit;
+
+#ifndef JIT_NUM_PAGE
+    #define JIT_NUM_PAGE 1
+#endif
+
+inline jit *jit_i(int np) {
+    jit *j = calloc(1, sizeof(jit));
+    j->np = np;
+    j->size = getpagesize() * np;
+    j->a = mmap(NULL, j->size, PROT_READ | PROT_WRITE | PROT_EXEC, MAP_ANONYMOUS | MAP_PRIVATE, -1, 0);
+    return j;
+}
+
+#ifndef JIT_NUM_PAGE_RSIZE
+    #define JIT_NUM_PAGE_RSIZE 2
+#endif
+
+inline void jit_f(jit *j) {
+    munmap(j->a, j->size);
+    free(j);
+}
+
+inline void jit_a(jit **j, uint8_t b) {
+    if ((*j)->len == (*j)->size) {
+        jit *nj = jit_i((*j)->np * JIT_NUM_PAGE_RSIZE);
+        nj->len = (*j)->len;
+        memcpy(nj->a, (*j)->a, nj->len);
+        jit_f(*j);
+        *j = nj;
+    }
+    (*j)->a[(*j)->len++] = b;
+}
+
+jit_stat jit_code(mod *const m, code *const c, jit **j);
+
+jit_stat jit_stk(mod *const m, fn_stk *const stk, jit **j);
