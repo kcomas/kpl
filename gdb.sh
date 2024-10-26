ba37b43e08b48e9af692981b82ea1c4eba0d4884
Kevin Comas <kevin@madnet.madnet>
2024-10-26 18:12:18 -0400

    test that gdb has correct args

--- gdb.sh
+++ gdb.sh
@@ -1,6 +1,22 @@
 #!/bin/bash
 
+# Usage ./gdb.sh *.kpl
+
 # access stack in gdb with
 # p *((var*)($rbp - (8 * 2))).sg
+
+set -e
+
+if [[ "$#" -ne 1 ]]
+then
+    echo "Usage: $0 file.kpl"
+    exit 1
+fi
+
+if [[ ! -f ./jit_test ]]
+then
+    make clean
+    make jit_test
+fi
 
 gdb -ex "b src/jit.c:`cat src/jit.c | grep -n "?? DEB" | cut -d":" -f 1`" -ex "r $1" ./jit_test
