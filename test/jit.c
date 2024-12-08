f702e632429657a28c514bf789676ca95586b069
Kevin Comas <kevin@madnet.madnet>
2024-12-08 01:09:16 -0500

    proper rex prefix

--- test/jit.c
+++ test/jit.c
@@ -46,8 +46,10 @@ static void rsub(uint8_t *m) {
     size_t p = 0;
     jit_push(&p, m, R(BP));
     jit_movrr(&p, m, R(BP), R(SP));
-    jit_movrr(&p, m, R(AX), R(DI));
-    jit_subrr(&p, m, R(AX), R(SI));
+    jit_movrr(&p, m, R(12), R(DI));
+    jit_subrr(&p, m, R(12), R(SI));
+    jit_movrr(&p, m, R(9), R(12));
+    jit_movrr(&p, m, R(AX), R(9));
     jit_pop(&p, m, R(BP));
     jit_ret(&p, m);
     printj(p, m);
