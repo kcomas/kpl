9ff985be926206897b9edc2191b055700dbb47cb
Kevin Comas <kevin@madnet.madnet>
2024-12-19 14:42:53 -0500

    reset tkn

--- src/lib/tkn.c
+++ src/lib/tkn.c
@@ -1,21 +1,3 @@
 
 #include "tkn.h"
 
-void tkn_st_i(tkn_st *const ts, const char *str, tbl *t) {
-    ts->lno = ts->cno = 1;
-    ts->pos = ts->ml = 0;
-    ts->str = str;
-    ts->t = t;
-}
-
-void tkn_st_n(tkn_st *const ts, bool incl) {
-    if (incl) {
-        ts->lno++;
-        ts->cno = 1;
-    } else ts->cno++;
-    ts->pos++;
-}
-
-void tkn_st_f(tkn_st *ts) {
-    tbl_f(ts->t);
-}
--- src/lib/tkn.h
+++ src/lib/tkn.h
@@ -2,34 +2,13 @@
 #pragma once
 
 #include "def.h"
-#include "tbl.h"
 
 #define TKN_STAT(N) TKN_STAT_##N
 
 typedef enum {
     TKN_STAT(OK),
+    TKN_STAT(NF),
+    TKN_STAT(INV),
     TKN_STAT(END)
 } tkn_stat;
 
-typedef struct {
-    size_t lno, cno, pos, ml; // line, char, pos, max tkn len
-    const char *str;
-    tbl *t;
-} tkn_st;
-
-void tkn_st_i(tkn_st *const ts, const char *str, tbl *t);
-
-void tkn_st_n(tkn_st *const ts, bool incl);
-
-typedef struct _tkn_d tkn_d;
-
-typedef tkn_stat tkn_g(tkn_st *const ts, const te *const tkn, size_t *const pos);
-
-// tkn te[sg;tkn_d]
-typedef struct _tkn_d {
-    size_t id;
-    ssize_t l; // -1 for variable
-    tkn_g *pf; // parse
-} tkn_d;
-
-void tkn_st_f(tkn_st *ts);
