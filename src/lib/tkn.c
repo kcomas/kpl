c1b4f0d30ad4efef29d4ea43a76aed1a44b00b22
Kevin Comas <kevin@madnet.madnet>
2024-12-21 22:51:43 -0500

    run time tokenizer

--- src/lib/tkn.c
+++ src/lib/tkn.c
@@ -43,12 +43,39 @@ void tkn_a(tkn *const t, const char *const s, ssize_t id, tkn_pf *pf) {
     te *kv;
     while (c.c.a != '\0') {
         if (tbl_g_i(tl, c, &kv) == TBL_STAT(NF)) return entry_add(t, tl, p, s, id, pf);
-        else tl = (tbl*) kv->d[3].p;
+        tl = (tbl*) kv->d[3].p;
         p = e + 1;
         c = c4_g(s, p, &e);
     }
     kv->d[1] = I6(id);
     kv->d[2].p = pf;
+}
+
+tkn_stat tkn_n(tkn *const t, te *const m) {
+    size_t e = 0;
+    m->d[0].i6 = -1;
+    m->d[1].u6 = t->lno;
+    m->d[2].u6 = t->cno;
+    m->d[3].u6 = t->pos;
+    m->d[4].u6 = 0;
+    if (t->pos >= t->s->l) return TKN_STAT(END);
+    un c = c4_g((char*) t->s->d, t->pos, &e);
+    if (c.c.a == '\0') return TKN_STAT(END);
+    tbl *tl = t->t;
+    tkn_pf *pf = t->df;
+    te *kv;
+    while (c.c.a != '\0') {
+        if (tbl_g_i(tl, c, &kv) == TBL_STAT(NF)) break;
+        tl = (tbl*) kv->d[3].p;
+        pf = (tkn_pf*) kv->d[2].p;
+        m->d[0] = kv->d[1];
+        t->pos = e + 1;
+        t->cno++;
+        m->d[4].u6 = t->pos;
+        if (t->pos >= t->s->l) break;
+        c = c4_g((char*) t->s->d, t->pos, &e);
+    }
+    return pf(t, m);
 }
 
 void tkn_f(tkn *t) {
