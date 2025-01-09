42ab4346f1e7d38c8eedce37563e5c8dfda1060a
Kevin Comas <kevin@madnet.madnet>
2025-01-09 12:29:34 -0500

    use parens for single ops

--- test/lib/psr.c
+++ test/lib/psr.c
@@ -40,9 +40,9 @@ int main(void) {
     te_f(ppnode(rpsr(psr_b("FN(I6;I6;FN(I6;I6))"))));
     te_f(ppnode(rpsr(psr_b("+(1;2)"))));
     //te_f(ppnode(rpsr(psr_b("+(1;2"))));
-    te_f(ppnode(rpsr(psr_b("f:FN(I6;I6;I6;I6)${a;b;c}#{a+b+c}"))));
+    te_f(ppnode(rpsr(psr_b("f:FN(I6;I6;I6;I6)$[a;b;c]#{d:a+b+c;-(d)}"))));
     te_f(ppnode(rpsr(psr_b("a:-(w) + -(x;y) + -z"))));
     te_f(ppnode(rpsr(psr_b("a(1;2)(3)()"))));
-    te_f(ppnode(rpsr(psr_b("{a`b`c\nd:#[1`x;2`y;3`z]}"))));
+    te_f(ppnode(rpsr(psr_b("{a`b`c\nd:#{1`x;{1+5-4}`y;3`z}}"))));
     return 0;
 }
