d63ff62dfb0fd3b55a77e321f1c721b1de98e676
Kevin Comas <kevin@madnet.madnet>
2025-02-13 10:48:33 -0500

    compare scalars

--- test/type.c
+++ test/type.c
@@ -4,4 +4,15 @@
 
 I;
 
-T(init, {});
+const alfr tm = { .a = malloc, .f = free };
+
+T(scalar, {
+    te *a = type_s_i(&tm, TYPE(I6));
+    te *b = type_s_i(&tm, TYPE(I6));
+    A(type_eq(a, b), "equal");
+    te *c = type_s_i(&tm, TYPE(F6));
+    A(!type_eq(b, c), "not equal");
+    te_f(a);
+    te_f(b);
+    te_f(c);
+});
