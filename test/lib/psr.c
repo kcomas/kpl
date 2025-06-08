f8ff7037c5d4ec44efffa00b95a65d6c966b6690
Kevin Comas <kevin.comas.git@gmail.com>
2025-06-08 17:27:33 -0400

    defer lst ex

--- test/lib/psr.c
+++ test/lib/psr.c
@@ -42,11 +42,12 @@ int main(void) {
     te_f(ppnode(rpsr(psr_b("-1+2"))));
     te_f(ppnode(rpsr(psr_b("{-+4;-(+4)}"))));
     //te_f(ppnode(rpsr(psr_b("+(1;2"))));
-    te_f(ppnode(rpsr(psr_b("f:FN(I6;I6;I6;I6)${a;b;c}##{-a+b+c}"))));
+    te_f(ppnode(rpsr(psr_b("f:FN(I6;I6;I6;I6)${`a;`b;`c}##{-a+b+c}"))));
     te_f(ppnode(rpsr(psr_b("a:-(w) + -(x;y) + -z"))));
     te_f(ppnode(rpsr(psr_b("a(1;2)(3)()"))));
     te_f(ppnode(rpsr(psr_b("{a`b`c\nd:#!{1`x;{1+5-4}`y;3`z}}"))));
     te_f(ppnode(rpsr(psr_b("#?{{c:2;a#?c:1;c}(a);{2}(b)}"))));
     te_f(ppnode(rpsr(psr_b("d#={{x}(x`v);0`e}"))));
+    te_f(ppnode(rpsr(psr_b("{a:1;b:2;{a+b+c}(3`c)}"))));
     return 0;
 }
