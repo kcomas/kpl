71224b9de1461e51aad85311e45c7ba9f1b8b961
Kevin Comas <kevin.comas.git@gmail.com>
2025-06-08 17:27:32 -0400

    generic vr

---
+++ src/vr.h
@@ -0,0 +1,34 @@
+
+#pragma once
+
+#include "def.h"
+
+#define VR_STAT(N) VR_STAT_##N
+
+typedef enum {
+    VR_STAT(OK),
+    VR_STAT(RES),
+    VR_STAT(SUB)
+} vr_stat;
+
+typedef struct _vr vr;
+
+typedef struct _vr {
+    ssize_t r;
+    size_t s, l;
+    alfn *af;
+    frfn *df, *ff;
+    var d[];
+} vr;
+
+vr *vr_i(size_t s, alfn *af, frfn *df, frfn *ff);
+
+#ifndef VR_RES
+    #define VR_RES 2
+#endif
+
+vr_stat vr_a(vr **v, var d);
+
+vr_stat vr_s(vr *const v, var *d);
+
+void vr_f(vr *v);
