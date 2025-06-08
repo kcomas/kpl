8e8a0c18cb5866637d90f720e74c06f23f6d4e9f
Kevin Comas <kevin.comas.git@gmail.com>
2025-06-08 17:27:34 -0400

    print ops for atg

--- src/atg_x64.c
+++ src/atg_x64.c
@@ -1,6 +1,44 @@
 
 #include "atg_x64.h"
 
+static atg_add_e_p(const tbl *t, size_t n, size_t idnt) {
+    n--;
+    for (size_t i = 0; i < idnt; i++) putchar(' ');
+    te *h = t->i->h;
+    while (h) {
+        te *kv = h->d[0].p;
+        uint16_t hc = u4_g_o(kv->d[0], 1);
+        uint16_t ht = u4_g_o(kv->d[0], 0);
+        printf("[%s %s]", ast_cls_str(hc), type_str(ht));
+        if (n > 0) {
+            putchar('\n');
+            atg_add_e_p(kv->d[1].p, n, idnt + 1);
+        }
+        h = h->d[2].p;
+    }
+}
+
+void atg_tbl_p(const tbl *t, ast_cls cls, size_t idnt) {
+    te *h = t->i->h;
+    while (h) {
+        te *kv = h->d[0].p;
+        uint16_t hc = u4_g_o(kv->d[0], 1);
+        uint16_t ht = u4_g_o(kv->d[0], 0);
+        putchar('(');
+        switch (cls) {
+            case AST_CLS(O):
+                printf("[%s %s]\n", ast_oc_str(hc), type_str(ht));
+                atg_add_e_p(kv->d[1].p, 2, idnt + 1);
+                break;
+            default:
+                printf("INV ATG P)\n");
+                return;
+        }
+        printf(")\n");
+        h = h->d[2].p;
+    }
+}
+
 static bool lst_cst_t(const te *an) {
     return an->d[4].u4 == OC(CST) && an->d[6].p && ((te*) an->d[6].p)->d[2].u4 == AST_CLS(L);
 }
@@ -39,7 +77,14 @@ static atg_stat lst_cst_e(atg *t, gen *g, te *restrict rn, te *restrict an, te *
     return ATG_STAT(OK);
 }
 
+
+static atg_stat add_i6_e_i6_e_i6(atg *t, gen *g, te *restrict rn, te *restrict an, te **e) {
+    HERE("TODO");
+    return ATG_STAT(INV);
+}
+
 atg *atg_b(atg *t) {
     atg_a_se(t, lst_cst_t, lst_cst_s, lst_cst_e);
+    atg_a_o(t, OC(ADD), TYPE(I6), AST_CLS(E), TYPE(I6), AST_CLS(E), TYPE(I6), add_i6_e_i6_e_i6);
     return t;
 }
--- src/atg_x64.h
+++ src/atg_x64.h
@@ -5,6 +5,8 @@
 #include "opt.h"
 #include "./lib/gen_x64.h"
 
+void atg_tbl_p(const tbl *t, ast_cls cls, size_t idnt);
+
 bool atg_x64_enq(const te *an);
 
 atg_stat atg_err(atg_stat stat, te *an, te **e);
