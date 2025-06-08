9218e76633378c67e34f4ed667e0e691b4640d23
Kevin Comas <kevin.comas.git@gmail.com>
2025-06-08 17:27:32 -0400

    pass filename to gdb

--- gdb.sh
+++ gdb.sh
@@ -3,4 +3,4 @@
 # access stack in gdb with
 # p *((var*)($rbp - (8 * 2))).sg
 
-gdb -ex "b src/jit.c:`cat src/jit.c | grep -n "?? DEB" | cut -d":" -f 1`" -ex "r ./ex/t/flt.kpl" ./jit_test
+gdb -ex "b src/jit.c:`cat src/jit.c | grep -n "?? DEB" | cut -d":" -f 1`" -ex "r $1" ./jit_test
