9010173045a76834656c1a4163d80b135395ac3c
Kevin Comas <kevin.comas.git@gmail.com>
2025-06-08 17:27:31 -0400

    check that mem in same chunk

--- test/al.c
+++ test/al.c
@@ -11,8 +11,17 @@ int main(void) {
     char *y = ala(a, 100000);
     strcpy(y, "This is a test");
     printf("%s\n", y);
+    char *z = ala(a, 50);
+    strcpy(z, "asdf");
+    printf("%s\n", z);
+    alci *xi = (void*) x - sizeof(alci);
+    alci *zi = (void*) z - sizeof(alci);
+    alci *yi = (void*) y - sizeof(alci);
+    printf("a->len: %lu\n", a->len);
+    printf("xzac: %d, xyac: %d, zyac: %d\n", xi->ac == zi->ac, xi->ac == yi->ac, zi->ac == yi->ac);
     alf(x);
     alf(y);
+    alf(z);
     al_f(a);
     return 0;
 }
