67f1c831d202d58c5793b00f806e54872d3241a6
Kevin Comas <kevin.comas.git@gmail.com>
2025-06-08 17:27:33 -0400

    write mem chunk

--- src/lib/mc.h
+++ src/lib/mc.h
@@ -9,13 +9,15 @@ typedef struct {
     size_t s, l; // size, len
     alfn *ma;
     frfn *mf;
-    uint8_t *d; // data in bytes
+    uint8_t d[]; // data in bytes
 } mc; // memory chunk
 
 mc *mc_i(size_t s, alfn *ma, frfn *mf);
 
 mc *mc_c(mc *m);
 
-void mc_wb(mc **m, uint8_t b);
+void mc_wa(mc **m, uint8_t b);
 
-void mv_f(mc *m);
+void mc_wb(mc **m, uint8_t *b, size_t l);
+
+void mc_f(mc *m);
