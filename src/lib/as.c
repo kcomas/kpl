f53c095d7d3cee4da02e64faafff6156e2ca2e4c
Kevin Comas <kevin.comas.git@gmail.com>
2025-06-08 17:27:33 -0400

    fn for setting lbls

--- src/lib/as.c
+++ src/lib/as.c
@@ -24,8 +24,8 @@ as *as_i(alfn *aa, frfn *af, frfn *lef, frfn *oef, frfn *cf, op_tbl_i *oti, tbl 
     return a;
 }
 
-static te *add_code(as *const a, code_id cid, size_t op_lbl_id, te *arg1, te *arg2, te *arg3, te *arg4, as_code_fn *fn) {
-    te *c = te_i(9, a->aa, a->cf);
+static te *add_code(as *const a, code_id cid, size_t op_lbl_id, te *arg1, te *arg2, te *arg3, te *arg4, as_code_fn *fn, as_code_fn *lbl_fn) {
+    te *c = te_i(10, a->aa, a->cf);
     c->d[0] = U6(cid);
     c->d[1] = U6(op_lbl_id == LABEL(UN) ? a->lc++ : op_lbl_id);
     c->d[2] = P(arg1);
@@ -33,14 +33,15 @@ static te *add_code(as *const a, code_id cid, size_t op_lbl_id, te *arg1, te *ar
     c->d[4] = P(arg3);
     c->d[5] = P(arg4);
     c->d[6] = P(fn);
-    // 7 pos
-    // 8 len
+    c->d[7] = P(lbl_fn);
+    // 8 pos
+    // 9 len
     lst_ab(a->code, P(c));
     return te_c(c);
 }
 
 static size_t add_lbl(as *const a, size_t lbl_id) {
-    te *c = add_code(a, CODE_ID(L), lbl_id, NULL, NULL, NULL, NULL, NULL);
+    te *c = add_code(a, CODE_ID(L), lbl_id, NULL, NULL, NULL, NULL, NULL, NULL);
     te *lbl = te_i(3, a->aa, a->lef);
     lbl->d[0] = c->d[1];
     lbl->d[1] = P(c);
@@ -54,6 +55,29 @@ size_t as_lbl_a(as *const a, size_t lbl_id) {
     if (lbl_id == LABEL(UN)) return add_lbl(a, lbl_id);
     if (tbl_g_i(a->lbls, U6(lbl_id), &kv) == TBL_STAT(OK)) return ((te*) kv->d[1].p)->d[0].u6;
     return add_lbl(a, lbl_id);
+}
+
+static tbl *add_op_entry(as *const a, tbl *const co, te **kv, size_t id) {
+    *kv = te_i(4, a->aa, a->oef);
+    (*kv)->d[0] = U6(id);
+    (*kv)->d[3] = P(a->oti());
+    tbl_a(co, *kv);
+    return (*kv)->d[3].p;
+}
+
+void as_op_a(as *const a, size_t op_id, arg_id ai1, arg_id ai2, arg_id ai3, arg_id ai4, as_code_fn *fn, as_code_fn *lbl_fn) {
+    tbl *co = a->ops;
+    te *kv;
+    if (tbl_g_i(co, U6(op_id), &kv) == TBL_STAT(NF)) co = add_op_entry(a, co, &kv, op_id);
+    else co = kv->d[3].p;
+    const arg_id args[] = {ai1, ai2, ai3, ai4};
+    for (size_t i = 0; i < 4; i++) {
+        if (args[i] == ARG_ID(N)) break;
+        if (tbl_g_i(co, U6(args[i]), &kv) == TBL_STAT(NF)) co = add_op_entry(a, co, &kv, args[i]);
+        else co = kv->d[3].p;
+    }
+    kv->d[1] = P(fn);
+    kv->d[2] = P(lbl_fn);
 }
 
 void as_f(as *a) {
--- src/lib/as.h
+++ src/lib/as.h
@@ -34,7 +34,7 @@ te *arg_i(arg_id id, un d, alfn *aa, frfn *af);
 
 // op te[op_id(must be user defined and unique);arg1;arg2;arg3;arg4]
 
-// op entry te[op_id;fn;tbl_args[te[arg_id;fn;tbl_args]]]
+// op entry te[op_id;fn;lbl_fn;tbl_args[te[arg_id;fn;lbl_fn;tbl_args]]]
 
 #define CODE_ID(N) CODE_ID_##N
 
@@ -43,9 +43,7 @@ typedef enum {
     CODE_ID(L) // label
 } code_id;
 
-typedef bool as_code_fn(te *arg1, te *arg2, te *arg3, te *arg4);
-
-// code te[code_id;op_or_label_id;arg_id1;arg_id2;arg_id3;arg_id4;fn;pos(in written bytes);len]
+// code te[code_id;op_or_label_id;arg_id1;arg_id2;arg_id3;arg_id4;fn;lbl_fn;pos(in written bytes);len]
 
 #define LABEL(N) LABEL_##N
 
@@ -59,7 +57,7 @@ typedef enum {
 
 typedef tbl *op_tbl_i(void);
 
-typedef struct {
+typedef struct _as {
     ssize_t r;
     size_t lc; // label counter
     alfn *aa;
@@ -69,12 +67,15 @@ typedef struct {
     lst *code;
 } as;
 
+typedef bool as_code_fn(as *const a, size_t *p, uint8_t *m, te *arg1, te *arg2, te *arg3, te *arg4);
+
 as *as_i(alfn *aa, frfn *af, frfn *lef, frfn *oef, frfn *cf, op_tbl_i *oti, tbl *lbls, lst *code);
 
 // LABEL(UN) for new label id at code
 size_t as_lbl_a(as *const a, size_t lbl_id);
 
-void as_op_a(as *const a, size_t op_id, arg_id ai1, arg_id ai2, arg_id ai3, arg_id ai4);
+// not using varardic to avoid extra checks
+void as_op_a(as *const a, size_t op_id, arg_id ai1, arg_id ai2, arg_id ai3, arg_id ai4, as_code_fn *fn, as_code_fn *lbl_fn);
 
 as_stat as_a(size_t op_id, te *arg1, te *arg2, te *arg3, te *arg4);
 
