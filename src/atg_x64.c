22f9042ae03e76889939dd1b61e6b7d33d6e70f7
Kevin Comas <kevin.comas.git@gmail.com>
2025-06-08 17:27:34 -0400

    add fns to queue

--- src/atg_x64.c
+++ src/atg_x64.c
@@ -6,7 +6,7 @@ bool atg_x64_enq(const te *an) {
         case AST_CLS(O):
             return an->d[4].u4 == OC(CST) && an->d[6].p && ((te*) an->d[6].p)->d[2].u4 == AST_CLS(L);
         case AST_CLS(L):
-            return true;
+            return an->d[0].p && ((te*) an->d[0].p)->d[2].u4 == AST_CLS(R);
         default:
             break;
     }
