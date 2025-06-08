d8c6b4ab12dc948253719a1857fb3c3344e1c1d0
Kevin Comas <kevin.comas.git@gmail.com>
2025-06-08 17:27:32 -0400

    dont inline tds

--- src/td.c
+++ src/td.c
@@ -9,8 +9,26 @@ extern inline void tdr_f(tdr *r, void *fn);
 
 extern inline tds *tds_i(void);
 
-extern inline void tds_a(tds *volatile s, tdr *const r);
+void tds_a(tds *volatile s, tdr *const r) {
+    sem_wait(&s->l);
+    er_c(r->e);
+    LST_A(s, r);
+    sem_post(&s->l);
+}
 
-extern inline tdr *tds_g(tds *volatile s, bool stk);
+tdr *tds_g(tds *volatile s, bool stk) {
+    sem_wait(&s->l);
+    tdr *r = NULL;
+    if (!s->h) {
+        s->total++;
+        r = tdr_i(s);
+        if (stk) tdr_stk_i(r);
+    } else {
+        LST_S(s, r);
+        if ((stk) && (!r->stk)) tdr_stk_i(r);
+    }
+    sem_post(&s->l);
+    return r;
+}
 
 extern inline void tds_f(tds *s);
--- src/td.h
+++ src/td.h
@@ -37,27 +37,9 @@ inline tds *tds_i(void) {
     return s;
 }
 
-inline void tds_a(tds *volatile s, tdr *const r) {
-    sem_wait(&s->l);
-    er_c(r->e);
-    LST_A(s, r);
-    sem_post(&s->l);
-}
+void tds_a(tds *volatile s, tdr *const r);
 
-inline tdr *tds_g(tds *volatile s, bool stk) {
-    sem_wait(&s->l);
-    tdr *r = NULL;
-    if (!s->h) {
-        s->total++;
-        r = tdr_i(s);
-        if (stk) tdr_stk_i(r);
-    } else {
-        LST_S(s, r);
-        if ((stk) && (!r->stk)) tdr_stk_i(r);
-    }
-    sem_post(&s->l);
-    return r;
-}
+tdr *tds_g(tds *volatile s, bool stk);
 
 inline void tds_f(tds *s) {
 #ifdef KPL_ALD // thread resources taken and returned
