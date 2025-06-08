81f3090b341d63aad2b9b397755c522138dcb411
Kevin Comas <kevin.comas.git@gmail.com>
2025-06-08 17:27:32 -0400

    add script to help deb

---
+++ gdb.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# access stack in gdb with
+# p *((var*)($rbp - (8 * 2))).sg
+
+gdb -ex "b src/jit.c:`cat src/jit.c | grep -n "?? DEB ??" | cut -d":" -f 1`" -ex "r ./ex/t/flt.kpl" ./jit_test
