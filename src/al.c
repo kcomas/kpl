c9cd74ec4f9c1e4104616bcb54f3252dd561dc82
Kevin Comas <kevin.comas.git@gmail.com>
2025-06-08 17:27:31 -0400

    use size_t for mmap size

--- src/al.c
+++ src/al.c
@@ -3,7 +3,13 @@
 
 extern inline al *al_i(void);
 
-extern inline alc *alc_i(al *const a, int size);
+extern inline alc *alc_i(al *const a, size_t size);
+
+void alc_f(alc *ac, void *fn) {
+    (void) fn;
+    munmap(ac->h, ac->size);
+    free(ac);
+}
 
 void *ala(al *const a, size_t size) {
     size += sizeof(alci);
@@ -21,4 +27,13 @@ void *ala(al *const a, size_t size) {
     ai->size = size;
     ai->ac = ac;
     return p + sizeof(alci);
+}
+
+void alf(void *ptr) {
+    alci *ai = (alci*) ptr - sizeof(alci);
+    alc *ac = ai->ac;
+    ac->aus -= ai->size;
+    if (ac->aus <= 0 && ((float) ac->len / (float) ac->size > ALC_USED_FREE_PCT)) {
+        LST_R(ac->a, alc, ac, alc_f, NULL);
+    }
 }
--- src/al.h
+++ src/al.h
@@ -18,20 +18,32 @@ inline al *al_i(void) {
     return calloc(1, sizeof(al));
 }
 
+#ifndef ALC_USED_FREE_PCT
+    #define ALC_USED_FREE_PCT 0.95
+#endif
+
 typedef struct _alc {
     al *a;
     alc *prev, *next;
-    int aus, len, size; // active used size, total used, size of chunk cmpt with getpagesize
+    size_t aus, len, size; // active used size, total used, size of chunk
     uint8_t *h; // memmap
 } alc; // allocator chunk no frees filled then freed
 
-inline alc *alc_i(al *const a, int size) {
+inline alc *alc_i(al *const a, size_t size) {
     alc *ac = calloc(1, sizeof(alc));
     ac->a = a;
     LST_A(a, ac);
-    ac->size = size <= getpagesize() ? 1 : ((int) size / getpagesize()) + 1;
+    size_t ps = (size_t) getpagesize();
+    ac->size = size <= ps ? ps : (size / ps + 1) * ps;
     ac->h = mmap(NULL, ac->size, PROT_READ | PROT_WRITE, MAP_ANONYMOUS | MAP_PRIVATE, -1, 0);
     return ac;
+}
+
+void alc_f(alc *ac, void *fn);
+
+inline void al_f(al *a) {
+    LST_F(a, alc, alc_f, NULL);
+    free(a);
 }
 
 typedef struct {
