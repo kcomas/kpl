0cd9e7080f33624c09f44f9cc31de66a9025db3e
Kevin Comas <kevin.comas.git@gmail.com>
2025-06-08 17:27:34 -0400

    chk_entry_f

--- src/lib/ast.h
+++ src/lib/ast.h
@@ -18,7 +18,7 @@ typedef enum {
     AST_CLS(R), // root te[tbl;te]
     AST_CLS(T), // type te[type]
     AST_CLS(E), // lst entry te[mc;id;type;node]
-    AST_CLS(I), // identifier [te[type];entry;mc]
+    AST_CLS(I), // identifier [mc]
     AST_CLS(S), // scalar type [un[type];un]
     AST_CLS(V), // vector type [te[type];lst]
     AST_CLS(O), // op [te[type];op_id;te;te]
