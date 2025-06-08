dce2bdf838c18291a5df61277ebfb22feadff99e
Kevin Comas <kevin.comas.git@gmail.com>
2025-06-08 17:27:34 -0400

    check tkn i6

--- src/ast.h
+++ src/ast.h
@@ -5,6 +5,15 @@
 #include "type.h"
 #include "psr.h"
 
+#define OP(N) OP_##N
+
+typedef enum {
+    OP(_START),
+    OP(ADD),
+    OP(SUB),
+    OP(_END)
+} op;
+
 ast *ast_b(ast *a);
 
 void ast_p(const te *an);
--- src/lib/ast.c
+++ src/lib/ast.c
@@ -17,14 +17,15 @@ const char *ast_cls_str(ast_cls cls) {
     return "INV";
 }
 
-ast *ast_i(const alfr *af, const alfr *na, psr_id_g pig, ast_tbl_i ati, ast_lst_i ali, tbl *t) {
+ast *ast_i(const alfr *af, const alfr *ta, psr_id_g pig, ast_tbl_i ati, ast_lst_i ali, tbl *pt, tbl *tt) {
     ast *a = af->a(sizeof(ast));
     a->af = af;
-    a->na = na;
+    a->ta = ta;
     a->pig = pig;
     a->ati = ati;
     a->ali = ali;
-    a->t = t;
+    a->pt = pt;
+    a->tt = tt;
     return a;
 }
 
@@ -123,7 +124,7 @@ te *ast_an_i(ast *a, te *restrict parent, te *restrict psr, ast_cls cls, un tt, 
         default:
             return NULL;
     }
-    te *an = te_i(len, a->na, nf);
+    te *an = te_i(len, a->ta, nf);
     an->d[0] = P(parent);
     an->d[1] = P(psr);
     an->d[2] = U6(cls);
@@ -137,11 +138,11 @@ te *ast_an_i(ast *a, te *restrict parent, te *restrict psr, ast_cls cls, un tt, 
 
 ast_stat ast_a(ast *a, size_t id, ast_tf atf) {
     te *kv;
-    if (tbl_g_i(a->t, U6(id), &kv) == TBL_STAT(OK)) return AST_STAT(INV);
-    kv = te_i(2, a->na, NULL);
+    if (tbl_g_i(a->pt, U6(id), &kv) == TBL_STAT(OK)) return AST_STAT(INV);
+    kv = te_i(2, a->ta, NULL);
     kv->d[0] = U6(id);
     kv->d[1] = P(atf);
-    tbl_a(a->t, kv);
+    tbl_a(a->pt, kv);
     return AST_STAT(OK);
 }
 
@@ -149,12 +150,23 @@ ast_stat ast_n(ast *a, te *pn, void **vn) {
     ssize_t pid = a->pig(pn);
     if (pid < 0) return AST_STAT(INV);
     te *kv;
-    if (tbl_g_i(a->t, U6(pid), &kv) == TBL_STAT(NF)) return AST_STAT(INV);
+    if (tbl_g_i(a->pt, U6(pid), &kv) == TBL_STAT(NF)) return AST_STAT(INV);
     ast_tf *atf = kv->d[1].p;
     return atf(a, pn, vn);
 }
 
+ast_stat ast_t_a(ast *a, size_t tid, size_t id) {
+    te *kv;
+    if (tbl_g_i(a->tt, U6(tid), &kv) == TBL_STAT(OK)) return AST_STAT(INV);
+    kv = te_i(2, a->ta, NULL);
+    kv->d[0] = U6(tid);
+    kv->d[1] = U6(id);
+    tbl_a(a->tt, kv);
+    return AST_STAT(OK);
+}
+
 void ast_f(ast *a) {
-    tbl_f(a->t);
+    tbl_f(a->pt);
+    tbl_f(a->tt);
     a->af->f(a);
 }
