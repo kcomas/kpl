8a04c1f0bbcabc9b07841b5e7d869b2454c05e84
Kevin Comas <kevin.comas.git@gmail.com>
2025-06-08 17:27:33 -0400

    basic lst test

--- test/lib/lst.c
+++ test/lib/lst.c
@@ -1,11 +1,28 @@
 
 #include "../../src/lib/lst.h"
+#include <stdio.h>
+
+static void lst_i6_p(const lst * const l) {
+    lst_itm *h = l->h;
+    while (h) {
+        printf("%ld ", h->d.i6);
+        h = h->n;
+    }
+    putchar('\n');
+}
 
 void basic(void) {
     lst *l = lst_i(&malloc, &malloc, &free, NULL, &free);
     lst_ab(l, I6(1));
     lst_ab(l, I6(2));
     lst_ab(l, I6(3));
+    lst_i6_p(l);
+    lst_af(l, I6(4));
+    lst_i6_p(l);
+    un u;
+    lst_sf(l, &u);
+    lst_sb(l, &u);
+    lst_i6_p(l);
     lst_f(l);
 }
 
