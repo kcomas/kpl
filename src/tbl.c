296417ebda9054743e302bafd7a2d6331b711063
Kevin Comas <kevin.comas.git@gmail.com>
2025-06-08 17:27:30 -0400

    tbl ops

--- src/tbl.c
+++ src/tbl.c
@@ -8,6 +8,56 @@ static size_t sdbm(const char *str) {
     return h;
 }
 
-extern inline tbl_itm *tbl_itm_i(const char *const str, void *data);
+extern inline tbl_itm *tbl_itm_i(const char *const str, void *const data);
 
 extern inline void tbl_itm_f(tbl_itm *ti, tbl_itm_data_f *fn);
+
+extern inline void tbl_f(tbl *t, tbl_itm_data_f *fn);
+
+#ifndef BUCKS_RSIZE
+    #define BUCKS_RSIZE 0.85 // bucks resize %
+#endif
+
+#ifndef BUCKS_RSIZE_MUL
+    #define BUCKS_RSIZE_MUL 2 // mul size by
+#endif
+
+#ifndef OPN_ADDR_SRCH
+    #define OPN_ADDR_SRCH 10 // bucks size / OPN_ADDR_SRCH searching
+#endif
+
+static void add_tbl(tbl *tl, tbl_itm **ins, tbl_itm *ti) {
+    *ins = ti;
+    if (!tl->h) tl->h = tl->h->next = tl->t = ti;
+    else tl->t = tl->t->next = ti;
+    tl->len++;
+}
+
+tbl_stat tbl_op(tbl **tl, const char *const str, void *const data, tbl_itm **ti, uint8_t op_flgs) {
+    // TODO resize
+    size_t h = sdbm(str) % (*tl)->size;
+    if ((*tl)->bucks[h] == NULL) {
+        if ((op_flgs & TBL_OP_FLG(FD)) || (op_flgs & TBL_OP_FLG(RM))) return TBL_STAT(NF);
+        *ti = tbl_itm_i(str, data);
+        add_tbl(*tl, &(*tl)->bucks[h], *ti);
+        return TBL_STAT(OK);
+    }
+    tbl_itm **b = &(*tl)->bucks[h];
+    while (*b) {
+        if (strcmp((*b)->str, str) == 0) {
+            if (op_flgs & TBL_OP_FLG(FD)) {
+                *ti = *b;
+                return TBL_STAT(OK);
+            } else if (op_flgs & TBL_OP_FLG(AD)) return TBL_STAT(AE);
+            else if (op_flgs & TBL_OP_FLG(RM)) {
+                // TODO remove
+                return TBL_STAT(OK);
+            }
+            (*b)->data = data;
+            return TBL_STAT(OK);
+        }
+        *b = (*b)->next;
+    }
+    if ((op_flgs & TBL_OP_FLG(FD)) || (op_flgs & TBL_OP_FLG(RM))) return TBL_STAT(NF);
+    // TODO add
+}
--- src/tbl.h
+++ src/tbl.h
@@ -3,6 +3,8 @@
 
 #include <stdlib.h>
 #include <string.h>
+#include <stdbool.h>
+#include <stdint.h>
 
 typedef struct _tbl_itm {
     struct _tbl_itm *next;
@@ -10,7 +12,7 @@ typedef struct _tbl_itm {
     char str[]; // null term
 } tbl_itm;
 
-inline tbl_itm *tbl_itm_i(const char *const str, void *data) {
+inline tbl_itm *tbl_itm_i(const char *const str, void *const data) {
     tbl_itm *ti = calloc(1, sizeof(tbl_itm) + sizeof(void*) + strlen(str) + 1);
     strcpy(ti->str, str);
     ti->data = data;
@@ -24,9 +26,41 @@ inline void tbl_itm_f(tbl_itm *ti, tbl_itm_data_f *fn) {
     free(ti);
 }
 
-#define BRS 0.85 // bucks resize %
-
 typedef struct {
-    size_t is, il, bs, bu; // itms size, itms len, bucks size, bucks used
-    tbl_itm **bucks, **itms;
-} tbl;
+    size_t size, len;
+    tbl_itm *h, *t, *bucks[];
+} tbl; // open address
+
+inline tbl* tbl_i(size_t size) {
+    tbl* tl = calloc(1, sizeof(tbl) + sizeof(tbl_itm*) * size);
+    tl->size = size;
+    return tl;
+}
+
+inline void tbl_f(tbl *tl, tbl_itm_data_f *fn) {
+    tbl_itm *h = tl->h;
+    while (h) {
+        tbl_itm *tmp = h;
+        h = h->next;
+        tbl_itm_f(tmp, fn);
+    }
+    free(tl);
+}
+
+#define TBL_STAT(N) TBL_STAT_##N
+
+typedef enum {
+   TBL_STAT(OK),
+   TBL_STAT(NF), // not found
+   TBL_STAT(AE) // already exists
+} tbl_stat;
+
+#define TBL_OP_FLG(N) TBL_OP_FLG_##N
+
+typedef enum {
+    TBL_OP_FLG(FD) = (1 << 0), // find
+    TBL_OP_FLG(AD) = (1 << 1), // add only
+    TBL_OP_FLG(RM) = (1 << 2) // remove
+} tbl_op_flg;
+
+tbl_stat tbl_op(tbl **tl, const char *const str, void *const data, tbl_itm **ti, uint8_t op_flgs);
