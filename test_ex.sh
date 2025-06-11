bf9cf571eb6010486268e7d721991d8846130b44
Kevin Comas <kevin.comas.git@gmail.com>
2025-06-10 21:30:17 -0400

    bash test ex

---
+++ test_ex.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# Test all programs in ex folder
+
+KPL=./kpl
+
+if [ ! -f $KPL ]
+then
+    make clean
+    make
+fi
+
+PGMS=$(find ./ex -type f -name "*.kpl")
+
+for P in $PGMS
+do
+    cat $P
+    $KPL $P
+    if [ $? -ne 0 ]
+    then
+        echo -e "\e[1;91mERROR\e[0m"
+        exit 1
+    fi
+done
+
+echo -e "\e[1;92mSUCCESS\e[0m"
