46379a2e06a40ad981d9d71ef133ab667cb8ff5d
Kevin Comas <kevin.comas.git@gmail.com>
2025-07-01 19:18:49 -0400

    chk all dfns in loop

--- src/atg_x64/dfnagn.c
+++ src/atg_x64/dfnagn.c
@@ -11,6 +11,16 @@ DFNES(i6, I6);
 DFNES(u6, U6);
 DFNES(f6, F6)
 
+static atg_stat da_in_loop(atg *t, gen *g, te *restrict an, err **e, te *restrict lte, const char *pf) {
+    te *tn = lte->d[2].p;
+    if (type_is_ref(tn->d[1].u4) && inloop(an)) {
+        void *fn = type_ref_g_des(tn->d[1].u4);
+        if (!fn) return atg_err(t, an, e, "atg inv des for define in loop");
+        if (gen_a(g, GEN_OP(CALL), gen_call_m(g, 1, var_arg(g, lte, type_g_x64_type(tn))), gen_data(g, X64_TYPE(M), P(fn)), NULL) != GEN_STAT(OK)) return atg_err(t, an, e, pf);
+    }
+    return ATG_STAT(OK);
+}
+
 static atg_stat dfnagn_e_e_(atg *t, gen *g, te *an, err **e) {
     te *l = ((te*) an->d[5].p)->d[3].p, *r = ((te*) an->d[6].p)->d[3].p;
     if (gen_a(g, GEN_OP(SET), var_arg(g, l, type_g_x64_type(l->d[2].p)), var_arg(g, r, type_g_x64_type(r->d[2].p)), NULL)) return atg_err(t, an, e, __FUNCTION__);
@@ -18,14 +28,11 @@ static atg_stat dfnagn_e_e_(atg *t, gen *g, te *an, err **e) {
 }
 
 static atg_stat dfn_t_e_t_oa_t(atg *t, gen *g, te *an, err **e) {
+    atg_stat stat;
     te *l = an->d[5].p, *r = atg_g_g(an->d[6].p)->d[1].p;
-    te *lt = ((te*) l->d[3].p)->d[2].p;
-    if (type_is_ref(lt->d[1].u4) && inloop(an)) {
-        void *fn = type_ref_g_des(lt->d[1].u4);
-        if (!fn) return atg_err(t, an, e, "atg inv des for define in loop");
-        if (gen_a(g, GEN_OP(CALL), gen_call_m(g, 1, var_arg(g, l->d[3].p, type_g_x64_type(lt))), gen_data(g, X64_TYPE(M), P(fn)), NULL) != GEN_STAT(OK)) return atg_err(t, an, e, __FUNCTION__);
-    }
-    if (gen_a(g, GEN_OP(SET), var_arg(g, l->d[3].p, type_g_x64_type(lt)), te_c(r), NULL)) return atg_err(t, an, e, __FUNCTION__);
+    te *lte = l->d[3].p;
+    if ((stat = da_in_loop(t, g, an, e, lte, __FUNCTION__)) != ATG_STAT(OK)) return stat;
+    if (gen_a(g, GEN_OP(SET), var_arg(g, lte, type_g_x64_type(lte->d[2].p)), te_c(r), NULL)) return atg_err(t, an, e, __FUNCTION__);
     return ATG_STAT(OK);
 }
 
@@ -40,8 +47,10 @@ static atg_stat dfn_fn_e_fn_s__g(atg *t, gen *g, te *an, err **e) {
 }
 
 static atg_stat dfn_tevrst_e_tevrst_vo_tevrst(atg *t, gen *g, te *an, err **e) {
+    atg_stat stat;
     te *lte = ((te*) an->d[5].p)->d[3].p;
     te *rgc = atg_g_g(an->d[6].p)->d[1].p;
+    if ((stat = da_in_loop(t, g, an, e, lte, __FUNCTION__)) != ATG_STAT(OK)) return stat;
     if (gen_a(g, GEN_OP(SET), gen_stkv(g, type_g_x64_type(lte->d[2].p), ast_lst_tbl_e_g_i(lte)), te_c(rgc), NULL)) return atg_err(t, an, e, __FUNCTION__);
     return ATG_STAT(OK);
 }
@@ -57,6 +66,7 @@ static atg_stat dfn_un_e_un_z(atg *t, gen *g, te *an, err **e) {
     if (lst_g_i(((tbl*) unt->d[2].p)->i, P(kv), &eid) != LST_STAT(OK)) return atg_err(t, an, e, "atg inv idx for un tbl itm");
     if (gen_a(g, GEN_OP(SET), gen_idx_m(g, X64_TYPE(N), 2, gen_tmp(g, X64_TYPE(M), ti), gen_data(g, X64_TYPE(U3), U3(offsetof(te, d)))), gen_data(g, X64_TYPE(U6), U6(eid)), NULL) != GEN_STAT(OK)) return atg_err(t, an, e, __FUNCTION__);
     if ((stat = v_set_fn(t, g, an, e, zn->d[4].p, ti, 1, atg_te_idx_d)) != ATG_STAT(OK)) return stat;
+    if ((stat = da_in_loop(t, g, an, e, lte, __FUNCTION__)) != ATG_STAT(OK)) return stat;
     if (gen_a(g, GEN_OP(SET), gen_stkv(g, X64_TYPE(M), ast_lst_tbl_e_g_i(lte)), gen_tmp(g, X64_TYPE(M), ti), NULL) != GEN_STAT(OK)) return atg_err(t, an, e, __FUNCTION__);
     return ATG_STAT(OK);
 }
